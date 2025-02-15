86ff048656fdf81321e97671fd39dbb489b1512a
gourab8389 <deyg6988@gmail.com>
2025-02-16 00:19:47 +0530

    refactor layout and typography: remove default body font, add Poppins and Larken Demo fonts, and update metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
       <h1 className="text-green2">hello</h1>
       <h1 className="text-green3">hello</h1>
       <h1 className="text-green4">hello</h1>
-      <h1 className="text-green5">hello</h1>
-      <h1 className="text-green6">hello</h1>
+      <h1 className="text-green5 font-poppins">hello</h1>
+      <h1 className="text-green6 font-larken-demo">hello</h1>
     </div>
   );
 }

908597687af6ae337747326dfffb5b3cefc3ab14
gourab8389 <deyg6988@gmail.com>
2025-02-16 00:37:58 +0530

    add LandingLayout and LandingPage components; remove old Home component

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,12 @@
+import { MaxWrapper } from "@/components/shared/max-wrapper";
+import React from "react";
+
+const LandingPage = () => {
+  return (
+  <MaxWrapper>
+        hello
+        </MaxWrapper>
+)
+};
+
+export default LandingPage;

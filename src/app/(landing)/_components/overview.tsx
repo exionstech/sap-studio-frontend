611739e7c91145903ab6547c22914523abff792f
gourab8389 <deyg6988@gmail.com>
2025-02-16 03:33:56 +0530

    add CustomIcon component; include star icon in OverView; update LandingPage to display OverView

--- src/app/(landing)/_components/overview.tsx
+++ src/app/(landing)/_components/overview.tsx
@@ -1,3 +1,4 @@
+import CustomIcon from "@/components/shared/custom-icon";
 
 const logos = [
     {
@@ -28,25 +29,26 @@ const logos = [
 
 const OverView = () => {
   return (
-    <div className="w-full max-w-screen-2xl relative mx-auto">
-      {/* Left smoke effect
-      <div className="absolute left-0 top-0 bottom-0 w-12 md:w-72 bg-gradient-to-r from-black via-black/70 to-transparent z-10" />
-      
-      {/* Right smoke effect */}
-      <div className="absolute right-0 top-0 bottom-0 w-12 md:w-72 z-10" />
+    <div className="w-full max-w-screen-2xl relative mx-auto mb-10">
       
       <div className="mx-auto w-full">
-        <div className="group mt-1 md:mt-4 flex gap-6 overflow-hidden py-2 md:py-4 bg-green1">
+      {/* Left smoke effect */}
+      <div className="absolute left-0 top-0 bottom-0 md:w-12 w-6 bg-gradient-to-r from-white via-green1/80 to-transparent z-10" />
+      
+      {/* Right smoke effect */}
+      <div className="absolute right-0 top-0 bottom-0 md:w-12 w-6 bg-gradient-to-l from-white via-green1/80 to-transparent z-10" />
+        <div className="group flex gap-6 overflow-hidden py-2 md:py-4 bg-green1">
           {Array(8)
             .fill(null)
             .map((_, index) => (
               <div
                 key={index}
-                className="flex shrink-0 animate-logo-cloud flex-row items-center gap-6"
+                className="flex shrink-0 animate-logo-cloud items-center gap-6"
               >
                 {logos.map((logo) => (
-                  <div key={logo.name} className="flex items-center gap-6 select-none pointer-events-none">
-                    
+                  <div key={logo.name} className="flex items-center gap-6 select-none pointer-events-none text-green4">
+                    <CustomIcon src={"/landing/star.svg"} size={10}/>
+                        <span className="text-lg font-medium">{logo.name}</span>
                   </div>
                 ))}
               </div>

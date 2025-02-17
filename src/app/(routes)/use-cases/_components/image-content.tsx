d07bae92a616f02265d71172faf42f76cbb07822
gourab8389 <deyg6988@gmail.com>
2025-02-18 02:02:23 +0530

    add CreativePersuits component and integrate ImageContent; update UseCasesPage layout

--- src/app/(routes)/use-cases/_components/image-content.tsx
+++ src/app/(routes)/use-cases/_components/image-content.tsx
@@ -2,32 +2,42 @@ import { cn } from "@/lib/utils";
 import Image from "next/image";
 
 interface ImageContentProps {
-        heading: string;
-        desc: string;
-        imgSrc: string;
-        className?: string;
+  heading: string;
+  desc: string;
+  imgSrc: string;
+  className?: string;
 }
 
-const ImageContent = (
-        { heading, desc, imgSrc, className }: ImageContentProps
-) => {
+const ImageContent = ({
+  heading,
+  desc,
+  imgSrc,
+  className,
+}: ImageContentProps) => {
   return (
-    <div className={cn("w-full flex flex-col md:flex-row gap-10 md:aspect-[3/1]", className)}>
-      <div className="w-full md:w-[35%] overflow-hidden">
+    <div
+      className={cn(
+        "w-full flex flex-col md:flex-row gap-10 md:aspect-[3/1]",
+        className
+      )}
+    >
+      <div className="w-full md:w-[35%] overflow-hidden flex items-center justify-center rounded-lg">
         <Image
-                src={imgSrc}
-                alt={heading}
-                width={500}
-                height={500}
-                className="object-cover shrink-0"
+          src={imgSrc}
+          alt={heading}
+          width={500}
+          height={500}
+          className="object-cover shrink-0"
         />
       </div>
-      <div className="w-full md:w-[65%] flex flex-col gap-5 p-5 justify-center">
-        <h2 className="text-3xl text-center md:text-start font-medium font-larken-demo text-green2">{heading}</h2>
-        <p className="text-center md:text-start">{desc}</p>
+      <div className="w-full bg-white md:w-[65%] flex flex-col gap-5 p-5 justify-center rounded-lg">
+        <h2 className="text-3xl text-center md:text-start font-medium font-larken-demo text-green4">
+          {heading}
+        </h2>
+        <p className="text-center md:text-start text-gray1">{desc}</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ImageContent;
--- src/app/(routes)/use-cases/page.tsx
+++ src/app/(routes)/use-cases/page.tsx
@@ -1,13 +1,11 @@
 import { MaxWrapper } from '@/components/shared/max-wrapper'
 import React from 'react'
+import CreativePersuits from './_components/creative-persuits'
 
 const UseCasesPage = () => {
   return (
-    <MaxWrapper>
-      <div className="min-h-screen flex items-center justify-center">
-      <h1>Use Cases</h1>
-      <p>This is the use cases page</p>
-      </div>
+    <MaxWrapper className='flex flex-col gap-14 mb-12 mt-[7rem]'>
+      <CreativePersuits/>
     </MaxWrapper>
   )
 }

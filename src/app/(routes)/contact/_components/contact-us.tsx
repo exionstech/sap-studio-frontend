aff0a3e101448a3da715c4f53474de6946ec13a2
gourab8389 <deyg6988@gmail.com>
2025-02-19 18:55:23 +0530

    enhance ContactForm with a link to the privacy policy; adjust padding in ContactUs section; improve layout and accessibility in ExperienceNow and Footer components

--- src/app/(routes)/contact/_components/contact-us.tsx
+++ src/app/(routes)/contact/_components/contact-us.tsx
@@ -5,7 +5,7 @@ import ContactForm from "./contact-us-form";
 const ContactUs = () => {
   return (
     <section className="w-full max-w-screen-2xl mx-auto px-5 md:px-14">
-      <div className="bg-green2 p-10 flex flex-col rounded-xl gap-10">
+      <div className="bg-green2 md:p-10 p-5 flex flex-col rounded-xl gap-10">
         <HeadingText
           section={"CONTACT US"}
           heading={"Let's Start Your Sustainability Journey"}

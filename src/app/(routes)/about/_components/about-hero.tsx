8989065ec3a635933787c0021da9060807cb6ec2
gourab8389 <deyg6988@gmail.com>
2025-02-19 19:18:05 +0530

    update AboutHero aspect ratio; modify Creative Pursuits section heading and description; enhance CopyRight component with a link; improve MobileNav functionality and styling; adjust PlantSignalComponent layout for better responsiveness

--- src/app/(routes)/about/_components/about-hero.tsx
+++ src/app/(routes)/about/_components/about-hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutHero = () => {
   return (
-    <section className='w-full bg-green2 aspect-[2/1] pb-[7rem]'>
+    <section className='w-full bg-green2 aspect-[2/3] md:aspect-[3/1] pb-[7rem]'>
         <div className="h-full flex items-center justify-center">
       <h1>About</h1>
       <p>This is the about page</p>

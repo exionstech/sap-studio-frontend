e187a03cf9b296e420267ac68667a68f15628855
gourab8389 <deyg6988@gmail.com>
2025-02-20 21:29:01 +0530

    refactor: enhance AboutHero component layout and content for improved presentation

--- src/app/(routes)/about/_components/about-hero.tsx
+++ src/app/(routes)/about/_components/about-hero.tsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const AboutHero = () => {
   return (
-    <section className='w-full bg-green2 aspect-[2/3] md:aspect-[3/1] pb-[7rem]'>
-        <div className="h-full flex items-center justify-center">
-      <h1>About</h1>
-      <p>This is the about page</p>
+    <section className="w-full bg-green2 pt-24">
+      <div className="w-full max-w-screen-2xl px-5 md:px-14 h-full flex flex-col gap-10 items-center justify-center mx-auto py-16">
+        <h1 className="text-5xl text-green4 font-medium font-larken-demo">Our Journey</h1>
+        <p className="text-green4 text-center">Sap Symphony began with a simple yet profound question: What if we could hear the sounds of plants? Driven by curiosity and a passion for nature, we set out to bridge the gap between technology and the natural world. Through extensive research and innovation, we developed a cutting-edge system that translates plant signals into harmonious sounds and real-time data, revealing a hidden layer of communication in nature. Collaborating with scientists, environmentalists, and tech pioneers, we refined our technology to create an immersive experience that allows people to listen, analyze, and connect with plants like never before. Today, Sap Symphony is more than just a product—it’s a movement toward a greener, more conscious world where humans and nature exist in harmony. As we continue to push boundaries, our vision remains clear: to empower individuals and industries with deeper insights into plant intelligence and to foster a future where technology and nature work hand in hand.</p>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default AboutHero
+export default AboutHero;
